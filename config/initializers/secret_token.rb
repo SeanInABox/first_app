4c22e9ff1090b7d5e346a7a3418ce38c74d277bf
Sean Carty <mrseanmcarty@gmail.com>
2012-06-26 15:20:06 -0400

    Add a full_title helper

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '1b8715f9aa3899b461c8570518081d348ee78e8a61cdb9354dc90dfad5160b119e3c135c2c99084e1a78c303ffd11572ac3b577f981eae8ce29cafc638c5b9b9'
